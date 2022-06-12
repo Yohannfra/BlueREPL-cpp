e13324bf86cd1145b54bf4f436ab05e70f0e0fa6
Yohann Assouline <yohannassouline@hotmail.fr>
2022-06-12 23:06:12 +0200

    Simplify write command and make it auto detect payload type

--- src/commands/write.hpp
+++ src/commands/write.hpp
@@ -6,47 +6,30 @@
 #include "utils.hpp"
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
 namespace Command {
     class Write : public ICommand {
+    private:
     public:
         Write()
         {
             _name = "write";
             _hint = "Write a characteristic";
-            _usage = "service_uuid characteristic_uuid [-a] [-x] [-d] value";
+            _usage = "service_uuid characteristic_uuid value";
 
             _args.add(
-                {"service_uuid", "\tuuid of the service in which the characteristic is", true, false, false});
-            _args.add({"characteristic_uuid", "uuid of the characteristic to read", true, false, false});
-            _args.add({"-a", "\t\twrite as ascii", false, true, true});
-            _args.add({"-x", "\t\twrite as hexadecimal (default)", false, true, true});
-            _args.add({"-d", "\t\twrite as decimal", false, true, true});
-            _args.add({"-b", "\t\twrite as binary", false, true, true});
+                {"service_uuid", "\tuuid of the service in which the characteristic is",
+                    true, false, false});
+            _args.add({"characteristic_uuid", "uuid of the characteristic to read", true,
+                false, false});
             _args.add({"value", "\t\tvalue to write", true, false, false});
         }
 
         int run(std::vector<std::string> &args, BleController &bt) override
         {
-            auto type = BleDataType::HEXADECIMAL;
-
-            for (auto &o : _args.filterOptions(args)) {
-                if (o == "-a") {
-                    type = BleDataType::ASCII;
-                } else if (o == "-x") {
-                    type = BleDataType::HEXADECIMAL;
-                } else if (o == "-d") {
-                    type = BleDataType::DECIMAL;
-                } else if (o == "-b") {
-                    type = BleDataType::BINARY;
-                } else {
-                    std::cerr << "Unknown options: " << o << std::endl;
-                    return EXIT_FAILURE;
-                }
-            }
-
             std::vector<std::string> values = _args.filterValues(args);
 
             if (values.size() != _args.getNumberOfRequiredArgs()) {
@@ -56,19 +39,26 @@ namespace Command {
 
             BluetoothUUID service = values.at(0);
             BluetoothUUID characteristic = values.at(1);
-            ByteArray value = values.at(2);
+            auto payloads = Utils::tokenizeLine(values.at(2));
 
-            if (type == BleDataType::ASCII) {
-                return bt.write(service, characteristic, value);
-            } else if (type == BleDataType::HEXADECIMAL) {
-                auto ret = Utils::parseHexStr(value);
-                return bt.write(service, characteristic, ret.value());
-            } else if (type == BleDataType::DECIMAL) {
-                auto ret = Utils::parseDecStr(value);
-                return bt.write(service, characteristic, ret.value());
-            } else if (type == BleDataType::BINARY) {
-                auto ret = Utils::parseBinStr(value);
-                return bt.write(service, characteristic, ret.value());
+            if (payloads.empty()) {
+                std::cerr << "Empty or invalid payload(s)" << std::endl;
+                return EXIT_FAILURE;
+            }
+
+            for (auto &[v, type] : payloads) {
+                if (type == BleDataType::ASCII) {
+                    return bt.write(service, characteristic, v);
+                } else if (type == BleDataType::HEXADECIMAL) {
+                    auto ret = Utils::parseHexStr(v);
+                    return bt.write(service, characteristic, ret.value());
+                } else if (type == BleDataType::DECIMAL) {
+                    auto ret = Utils::parseDecStr(v);
+                    return bt.write(service, characteristic, ret.value());
+                } else if (type == BleDataType::BINARY) {
+                    auto ret = Utils::parseBinStr(v);
+                    return bt.write(service, characteristic, ret.value());
+                }
             }
 
             return EXIT_SUCCESS;
